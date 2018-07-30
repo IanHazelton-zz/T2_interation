65644c9a7749832d10a2ea6db38b3cd1f549c9ce
Ian Hazelton <ian.hazelton@gmail.com>
2018-07-30 16:05:15 +0200

    iteration 1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,15 +4,31 @@ $('#spinner').click(function(e){
     $('.filter, .wrap__popup, .spinner').addClass('--active');
   });
   
+//add success spinner
+$('#spinnerSuccess').click(function(e){
+  e.preventDefault();
+  $('.filter, .wrap__popup, .resultSpinner__wrap').addClass('--active');
+  $('#js-progress-bar').addClass('--complete');
+  setTimeout(function(){$('#js-success-tick, #js-success-ring').addClass('--complete');},2000);
+});
+
+  
 //add printer
 $('#printer').click(function(e){
     e.preventDefault();
     $('.filter, .wrap__popup, .printer').addClass('--active');
   });
   
+//add barcode scanner
+$('#barcode').click(function(e){
+  e.preventDefault();
+  $('.filter, .wrap__popup, .wrap__scanner').addClass('--active');
+}); 
+
 ////////////////////////////////////////////////////////////////////////  
 //Remove all
 $('#filter').click(function(e){
     e.preventDefault();
-    $('.filter, .wrap__popup, .spinner, .printer').removeClass('--active');
+    $('.filter, .wrap__popup, .spinner, .printer, .wrap__scanner, .spinnerSuccess').removeClass('--active');
   });
+
