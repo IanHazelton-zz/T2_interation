ef99b466add5ade0dbf52a271adb136b5bb80341
Ian Hazelton <ian.hazelton@gmail.com>
2018-10-11 16:17:47 +0200

    First POC

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,38 +1,63 @@
-//add spinner
-$('#spinner').click(function(e){
-    e.preventDefault();
-    $('.filter, .wrap__popup, .spinner').addClass('--active');
-  });
-  
-//add success spinner
-$('#spinnerSuccess').click(function(e){
-  e.preventDefault();
-  $('#js-resultSpinner').removeClass('--spinner-complete');
-  $('.filter, .wrap__popup, .resultSpinner__wrap').addClass('--active');
-  $('#js-progress-bar').addClass('--complete');
-  setTimeout(function(){$('#js-success-tick, #js-success-ring').addClass('--complete');},1700);
-  setTimeout(function(){$('#js-resultSpinner').addClass('--spinner-complete');},1800);
-});
 
-  
-//add printer
-$('#printer').click(function(e){
-    e.preventDefault();
-    $('.filter, .wrap__popup, .printer').addClass('--active');
-  });
-  
-//add barcode scanner
-$('#barcode').click(function(e){
-  e.preventDefault();
-  $('.filter, .wrap__popup, .barcode').addClass('--active');
-}); 
+//debug buttons
+var btnUser = document.getElementById('btnUser');
+var btnSrc = document.getElementById('btnSrc');
+var btnInfo = document.getElementById('btnInfo');
+var btnApps = document.getElementById('btnApps');
+var btnGuide = document.getElementById('btnGuide');
+var btnUp = document.getElementById('btnUp');
+var btnRight = document.getElementById('btnRight');
+var btnDown = document.getElementById('btnDown');
+var btnLeft = document.getElementById('btnLeft');
+var btnOk = document.getElementById('btnOk');
+var btnReturn = document.getElementById('btnReturn');
+var btnHome = document.getElementById('btnHome');
+var btnMic = document.getElementById('btnMic');
+var btnPrgUp = document.getElementById('btnPrgUp');
+var btnPrgDown = document.getElementById('btnPrgDown');
 
-////////////////////////////////////////////////////////////////////////  
-//Remove all
-$('#filter').click(function(e){
-    e.preventDefault();
-    $('.filter, .wrap__popup, .spinner, .printer, .barcode, .resultSpinner__wrap').removeClass('--active');
-    $('.notify__progress-bar, .success-tick, .success-ring').removeClass('--complete');
-
-  });
-
+btnUser.onclick = function() {
+    console.log('User button clicked');
+};
+btnSrc.onclick = function() {
+    console.log('Source button clicked');
+};
+btnInfo.onclick = function() {
+    console.log('Info button clicked');
+};
+btnApps.onclick = function() {
+    console.log('Apps button clicked');
+};
+btnGuide.onclick = function() {
+    console.log('Guide button clicked');
+};
+btnUp.onclick = function() {
+    console.log('Up button clicked');
+};
+btnRight.onclick = function() {
+    console.log('Right button clicked');
+};
+btnDown.onclick = function() {
+    console.log('Down button clicked');
+};
+btnLeft.onclick = function() {
+    console.log('Left button clicked');
+};
+btnOk.onclick = function() {
+    console.log('OK button clicked');
+};
+btnReturn.onclick = function() {
+    console.log('Return button clicked');
+};
+btnHome.onclick = function() {
+    console.log('Home button clicked');
+};
+btnMic.onclick = function() {
+    console.log('Microphone button clicked');
+};
+btnPrgUp.onclick = function() {
+    console.log('Program - Up button clicked');
+};
+btnPrgDown.onclick = function() {
+    console.log('Program - Down button clicked');
+};
